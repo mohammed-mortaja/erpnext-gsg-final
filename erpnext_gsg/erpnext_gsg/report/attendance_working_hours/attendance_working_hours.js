0f2d8f672716a92641ce8bfd1ede5a74fc53edf1
mohammed_mortaja <sala.moh611@gmail.com>
2023-03-07 10:43:26 +0200

    last commit

---
+++ erpnext_gsg/erpnext_gsg/report/attendance_working_hours/attendance_working_hours.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Attendance Working Hours"] = {
+	"filters": [
+		{'fieldname': 'from_date', 'label': 'From Date', 'fieldtype': 'Date'},
+		{'fieldname': 'to_date', 'label': 'To Date', 'fieldtype': 'Date'},
+		{'fieldname': 'employee', 'label': 'Employee', 'fieldtype': 'Link', 'options': 'Employee'},
+		{'fieldname': 'department', 'label': 'Department', 'fieldtype': 'Link', 'options': 'Department'},
+
+	]
+};
---
+++ erpnext_gsg/erpnext_gsg/report/attendance_working_hours/attendance_working_hours.py
@@ -0,0 +1,62 @@
+import datetime
+from frappe.utils import time_diff_in_hours, get_url
+import frappe
+
+def calculate_working_hours(check_in, check_out):
+    if not check_out or not check_in:
+        return 0.0
+
+    time_diff = time_diff_in_hours(check_out, check_in)
+
+    return time_diff
+
+def execute(filters=None):
+    columns, data = [], []
+
+    columns=[
+        {'fieldname': 'attendance_date', 'label': 'Attendance Date', 'fieldtype': 'Date'},
+        {'fieldname': 'employee', 'label': 'Employee', 'fieldtype': 'Link', 'options': 'Employee'},
+        {'fieldname': 'employee_name', 'label': 'Employee Name', 'fieldtype': 'Data'},
+        {'fieldname': 'check_in', 'label': 'Check In', 'fieldtype': 'Time'},
+        {'fieldname': 'check_out', 'label': 'Check Out', 'fieldtype': 'Time'},
+        {'fieldname': 'working_hours', 'label': 'Working Hours', 'fieldtype': 'Float'},
+		{'fieldname': 'view_attendance', 'label': 'View Attendance', 'fieldtype': 'Link','options': 'Attendance'}
+    ]
+
+    # Get the filter values
+    from_date = filters.get('from_date')
+    to_date = filters.get('to_date')
+    employee = filters.get('employee')
+    department = filters.get('department')
+
+    # Build the SQL query
+    query = """
+        SELECT `name`, `attendance_date`, `employee`, `employee_name`, `check_in`, `check_out`
+        FROM `tabAttendance`
+        WHERE `employee` = %s
+        AND `attendance_date` BETWEEN %s AND %s
+    """
+    params = [employee, from_date, to_date]
+
+    if department:
+        query += " AND `department` = %s"
+        params.append(department)
+
+    # Execute the query and retrieve the results
+    results = frappe.db.sql(query, tuple(params), as_dict=True)
+
+    # Process the results and build the report data
+    for result in results:
+        attendance_form_url = get_url('/desk#Form/Attendance/{0}'.format(result.name))
+        working_hours = calculate_working_hours(result.check_in, result.check_out)
+        data.append([
+            result.attendance_date,
+            result.employee,
+            result.employee_name,
+            result.check_in,
+            result.check_out,
+            working_hours,
+			'<a href="{0}" target="_blank">View Attendance</a>'.format(attendance_form_url)
+        ])
+
+    return columns, data
