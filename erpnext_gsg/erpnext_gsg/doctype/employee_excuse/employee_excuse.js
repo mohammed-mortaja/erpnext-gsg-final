e18a2ef7a89afeb6b6f9dbb8bec31c7b378a8830
mohammed_mortaja <sala.moh611@gmail.com>
2023-03-07 12:23:11 +0200

    last commit

---
+++ erpnext_gsg/erpnext_gsg/doctype/employee_excuse/employee_excuse.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, mohammed-mortaja and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Employee Excuse', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ erpnext_gsg/erpnext_gsg/doctype/employee_excuse/test_employee_excuse.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, mohammed-mortaja and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestEmployeeExcuse(unittest.TestCase):
+	pass
